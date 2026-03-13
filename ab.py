5e44973237f74f3c049bf2cbe8104ae91a377f80
akumahanzo <akuma.hanzo1@gmail.com>
2026-03-13 13:42:43 +0000

    Update ab.py

--- ab.py
+++ ab.py
@@ -1,1 +1,9 @@
+def traiter_rapprochement(fichier_releve, fichier_fea):
+    
+    # ton traitement pandas
 
+    chemin_sortie = "rapport.xlsx"
+
+    df.to_excel(chemin_sortie)
+
+    return chemin_sortie
