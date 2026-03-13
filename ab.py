7baa5ff03fd708e1d3fa50122eab274be80916b7
akumahanzo <akuma.hanzo1@gmail.com>
2026-03-13 13:44:35 +0000

    Update ab.py

--- ab.py
+++ ab.py
@@ -1,9 +1,30 @@
+import pandas as pd
+from io import BytesIO
+
+
 def traiter_rapprochement(fichier_releve, fichier_fea):
-    
-    # ton traitement pandas
 
-    chemin_sortie = "rapport.xlsx"
+    # Lecture des fichiers
+    df_releve = pd.read_excel(fichier_releve)
+    df_fea = pd.read_excel(fichier_fea)
 
-    df.to_excel(chemin_sortie)
+    # Exemple de rapprochement simple (à adapter avec ta logique)
+    df_resultat = pd.merge(
+        df_releve,
+        df_fea,
+        how="outer",
+        indicator=True
+    )
 
-    return chemin_sortie
+    # Création du fichier Excel en mémoire
+    output = BytesIO()
+
+    with pd.ExcelWriter(output, engine="xlsxwriter") as writer:
+
+        df_releve.to_excel(writer, sheet_name="Releve", index=False)
+        df_fea.to_excel(writer, sheet_name="FAE", index=False)
+        df_resultat.to_excel(writer, sheet_name="Rapprochement", index=False)
+
+    output.seek(0)
+
+    return output
